79948f6692047efc372f8e972f34287ac3fc174a
Va1ant <92823101+Va1ant@users.noreply.github.com>
2021-10-24 16:24:44 +0300

    Update Macro.h

--- Macro.h
+++ Macro.h
@@ -1,16 +1,27 @@
+#ifndef Macro_h
+#define Macro_h
+
 #define pinAsOutput(pin) DDRB |= (1 << pin)
 #define pinAsInput(pin)  DDRB &= ~(1 << pin)
 #define pinToLOW(pin)    PORTB &=  ~(1 << pin)
 #define pinToHIGH(pin)   PORTB |= (1 << pin)
 #define readPin(pin)     ((PINB & (1 << pin)) ? 1 : 0)
 
-#define HEATER_PIN  PB3
-#define TERMO_PIN   PB4
-#define ERROR_PIN   PB2
+#define HEATER_PIN PB3
+#define TERMO_PIN PB4
+//#define ERROR_PIN PB2
 
-#define DO_ERROR {\
+#define DO_ERROR(pin) {\
   pinToLOW(HEATER_PIN);\
-  pinAsOutput(ERROR_PIN);\
-  pinToHIGH(ERROR_PIN);\
+  pinAsOutput(pin);\
+  pinToHIGH(pin);\
   continue;\
 }
+
+#define HAPPY_END {\
+  pinToLOW(PB0);\
+  pinToLOW(PB1);\
+  pinToLOW(PB2);\
+}
+
+#endif
