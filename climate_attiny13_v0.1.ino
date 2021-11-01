1524575b73b72eb87772ac5e2d3495d49d8d44c1
Va1ant <92823101+Va1ant@users.noreply.github.com>
2021-11-01 13:26:30 +0300

    Update climate_attiny13_v0.1.ino

--- climate_attiny13_v0.1.ino
+++ climate_attiny13_v0.1.ino
@@ -4,38 +4,39 @@
 #include <util/delay.h>
 #include "Macro.h"
 #include "OneWireTiny.h"
-#include "RelayTiny.h"
+#include "RelayTinyStruct.h"
 
 int main(void) {
-  uint8_t data[9]; //[9] for crc; [7] if normal work
-  GyverRelay heater;
+  uint8_t data[9]; //[9] for crc; [7] if normal work; [2] for 0.5 temperature resolution
+
+  struct Relay heater;
 
   heater.setpoint = 21 * 16;  // желаемая температура
   heater.hysteresis = 1 * 16; // +- от желаемой
   heater.k = 1;               // коэффициент обратной связи
+  heater.prevInput = 0;
+  heater.output = 0;
+  heater.direction = 1;
 
   pinAsOutput(HEATER_PIN);
   pinToLOW(HEATER_PIN);
 
-  while (1) {
-    if (!ds_reset()) DO_ERROR(PB2);
-    ds_write(0xCC);
-    ds_write(0x44); //make (0x44, 1) if parasite power, but be carefull, look at line 33 this file
+  for (;;) {
+    if (!ds_termo(0x44)) DO_ERROR(PB2); // compute command or termometer not on line
 
-    _delay_ms(1000);
+    _delay_ms(1000);                    // time to compute temperature
 
-    if (!ds_reset()) DO_ERROR(PB2);
-    ds_write(0xCC);
-    ds_write(0xBE);
+    if (!ds_termo(0xBE)) DO_ERROR(PB2); // read command or termometer not on line
 
     for (uint8_t i = 0; i < 9; i++) data[i] = ds_read(); //[9] for crc; [7] if normal work
 
     if (data[0] == 0xAA) DO_ERROR(PB1); // if true, maybe problem with power on termometer
-    if (ds_crc8(data, 8) != data[8]) DO_ERROR(PB0); //check crc; delete this if normal work
+    if (ds_crc(data, 8) != data[8]) DO_ERROR(PB0); //check crc; delete if work fine
 
+    // calculate temperature and send it to relay regulator
     heater.input = ((data[0] << 3 | data[1] << 11) & 0xFFF0) + 12 - data[6];
 
-    if (heater.compute()) pinToHIGH(HEATER_PIN);
+    if (compute(&heater)) pinToHIGH(HEATER_PIN);
     else pinToLOW(HEATER_PIN);
 
     HAPPY_END;
