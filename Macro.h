7dbe1ca94ffab0f1f7f746e92e6cc536e5dcdb73
Va1ant <arhipovich.val@gmail.com>
2021-12-18 14:18:16 +0300

    Update
    - improved data transfer procedure
    - improved wdt configuration

--- Macro.h
+++ Macro.h
@@ -2,10 +2,10 @@
 #define MACRO_H_
 
 #define pinAsOutput(pin)	DDRB |= (1 << pin)
-#define pinAsInput(pin)	DDRB &= ~(1 << pin)
-#define pinToLow(pin)	PORTB &=  ~(1 << pin)
-#define pinToHigh(pin)	PORTB |= (1 << pin)
-#define pinInvert(pin)	PORTB ^= (1 << pin)
+#define pinAsInput(pin)		DDRB &= ~(1 << pin)
+#define pinToLow(pin)		PORTB &=  ~(1 << pin)
+#define pinToHigh(pin)		PORTB |= (1 << pin)
+#define pinInvert(pin)		PORTB ^= (1 << pin)
 #define readPin(pin)		((PINB & (1 << pin)) ? 1 : 0)
 
 #define ERROR_PIN	PB1
